4881b645b71cbf0b9de5df6e721e99eeca054d13
rajusingh <raju.singh41@gmail.com>
2020-02-03 20:00:18 +0530

    add db

---
+++ DashboardController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeadManagment.WebUI
+{
+    [Area("Master")]
+    [Authorize(Roles = "Admin,User")]
+    public class DashboardController : BaseController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selectedDate"></param>
+        /// <returns></returns>
+        public IActionResult TodayScheduleFollowUp(DateTime? selectedDate)
+        {
+            var date = selectedDate.HasValue ? selectedDate.Value.Date : DateTime.Now.Date;
+            if (User.IsInRole("Admin"))
+            {
+
+                var todaySchedule = (from al in _dbContext.AssingLeads
+                                     where (al.LeadStatus != 4 && al.LeadStatus != 6 && al.LeadStatus != 3)
+                                     join fd in (from ffd in _dbContext.LeadFollowUpDetails
+                                                 where (ffd.FollowUpDate != null && ffd.FollowUpDate.Value.Date == date)
+                                                 group ffd by ffd.FkAssignLeadId into gg
+                                                 select new
+                                                 {
+                                                     assignId = gg.Key,
+                                                     fid = gg.Max(x => x.PkLeadFollowUpId)
+                                                 }) on al.AssignLeadId equals fd.assignId
+                                     join lm in _dbContext.LeadMaster on al.FkLeadId equals lm.Id
+                                     join us in _dbContext.Users on al.FkUserId equals us.UserId
+                                     join foll in _dbContext.LeadFollowUpDetails on fd.fid equals foll.PkLeadFollowUpId
+                                     select new
+                                     {
+
+                                         name = $"{us.FirstName } {us.LastName } ",
+                                         senderName = lm.Sendername,
+                                         lastComment = foll.FollowUpComment ?? string.Empty,
+                                         time = foll.FollowUpDate.HasValue ? foll.FollowUpDate.Value.ToString("HH:mm") : "[N/A]",
+                                     }).ToList();
+                return Ok(todaySchedule);
+            }
+            else
+            {
+                var todaySchedule = (from al in _dbContext.AssingLeads
+                                     where (al.FkUserId == Convert.ToInt64(User.FindFirstValue("UserId")) && (al.LeadStatus != 4 && al.LeadStatus != 6 && al.LeadStatus != 3))
+                                     join fd in (from ffd in _dbContext.LeadFollowUpDetails
+                                                 where (ffd.FollowUpDate != null && ffd.FollowUpDate.Value.Date == date)
+                                                 group ffd by ffd.FkAssignLeadId into gg
+                                                 select new
+                                                 {
+                                                     assignId = gg.Key,
+                                                     fid = gg.Max(x => x.PkLeadFollowUpId)
+                                                 }) on al.AssignLeadId equals fd.assignId
+                                     join lm in _dbContext.LeadMaster on al.FkLeadId equals lm.Id
+                                     join us in _dbContext.Users on al.FkUserId equals us.UserId
+                                     join foll in _dbContext.LeadFollowUpDetails on fd.fid equals foll.PkLeadFollowUpId
+                                     select new
+                                     {
+                                         name = $"{lm.Sendername }  [<a href='mailto:#'>{lm.Senderemail }</a>]",
+                                         lastComment = foll.FollowUpComment ?? string.Empty,
+                                         time = foll.FollowUpDate.HasValue ? foll.FollowUpDate.Value.ToString("HH:mm") : "[N/A]",
+                                     }).ToList();
+                return Ok(todaySchedule);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> TodayLeads()
+        {
+            //if (User.IsInRole("Admin"))
+            //{
+            //    var unassignLeads = await _dbContext.LeadMaster.Where(x => (x.Isassigned ?? false) == false).OrderByDescending(x => x.LogTime).ToListAsync();
+            //    return Ok(unassignLeads);
+            //}
+            //else
+            //{
+            //    var todayLeads = await _dbContext.LeadMaster.Where(x => (x.CreateDate != null && x.CreateDate.Value.Date == DateTime.Now.Date) && (x.Isassigned ?? false) == false).ToListAsync();
+            //    return Ok(todayLeads);
+            //}
+            var unassignLeads = await _dbContext.LeadMaster.Where(x => (x.Isassigned ?? false) == false).OrderByDescending(x => x.LogTime).ToListAsync();
+            return Ok(unassignLeads);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> MissedFollowup()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var todaySchedule = (from al in _dbContext.AssingLeads
+                                     where ((al.LeadStatus ?? 0) == 2 || (al.LeadStatus ?? 0) == 5)
+                                     join fd in (from ffd in _dbContext.LeadFollowUpDetails
+                                                 where (ffd.FollowUpDate != null && ffd.FollowUpDate.Value.Date < DateTime.Now.Date)
+                                                 group ffd by ffd.FkAssignLeadId into gg
+                                                 select new
+                                                 {
+                                                     assignId = gg.Key,
+                                                     fid = gg.Max(x => x.PkLeadFollowUpId)
+                                                 }) on al.AssignLeadId equals fd.assignId
+                                     join lm in _dbContext.LeadMaster on al.FkLeadId equals lm.Id
+                                     join us in _dbContext.Users on al.FkUserId equals us.UserId
+                                     join foll in _dbContext.LeadFollowUpDetails on fd.fid equals foll.PkLeadFollowUpId 
+                                     orderby foll.FollowUpDate descending
+                                     select new
+                                     {
+                                         name = $"{us.FirstName } {us.LastName } ",
+                                         senderName = lm.Sendername,
+                                         time = foll.FollowUpDate.HasValue ? foll.FollowUpDate.Value.ToString("dd/MM/yyyy HH:mm") : "[N/A]",
+                                     }).ToList();
+                return Ok(todaySchedule);
+            }
+            else
+            {
+                var todaySchedule = (from al in _dbContext.AssingLeads
+                                     where (((al.LeadStatus ?? 0) == 2 || (al.LeadStatus ?? 0) == 5) && al.FkUserId== Convert.ToInt64(User.FindFirstValue("UserId")))
+                                     join fd in (from ffd in _dbContext.LeadFollowUpDetails
+                                                 where (ffd.FollowUpDate != null && ffd.FollowUpDate.Value.Date < DateTime.Now.Date)
+                                                 group ffd by ffd.FkAssignLeadId into gg
+                                                 select new
+                                                 {
+                                                     assignId = gg.Key,
+                                                     fid = gg.Max(x => x.PkLeadFollowUpId)
+                                                 }) on al.AssignLeadId equals fd.assignId
+                                     join lm in _dbContext.LeadMaster on al.FkLeadId equals lm.Id
+                                     join us in _dbContext.Users on al.FkUserId equals us.UserId
+                                     join foll in _dbContext.LeadFollowUpDetails on fd.fid equals foll.PkLeadFollowUpId
+                                     orderby foll.FollowUpDate descending
+                                     select new
+                                     {
+                                         name = lm.Sendername,
+                                         lastComment = foll.FollowUpComment,
+                                         time = foll.FollowUpDate.HasValue ? foll.FollowUpDate.Value.ToString("dd/MM/yyyy HH:mm") : "[N/A]",
+                                     }).ToList();
+                return Ok(todaySchedule);
+            }
+        }
+    }
+}
---
+++ InProgressLeadController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeadManagment.WebUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Area("Master")]
+    [Authorize(Roles = "Admin,User")]
+    public class InProgressLeadController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var model = new DataTableViewModel()
+            {
+                ID = "TblInProgressLead",
+                PageTitle = "InProgress Lead",
+                Headers = new List<string> { "no-sort_Id","FollowUp Status","FollowUp Remarks","Next FollowUp Date",
+                                             "Sender Name", "Sender Email", "Mobile", "Subject","Quotation"
+                                            // , "Enq Date Time"
+                                             ,"no-sort_View"},
+                JsonUrl = Url.Action("InProgressLeadDataTable", "InProgressLead", new { area = "Master" })
+            };
+            return View(model);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult InProgressLeadView()
+        {
+            var model = new DataTableViewModel()
+            {
+                ID = "TblInProgressLead",
+                PageTitle = "InProgress Lead",
+                Headers = new List<string> { "no-sort_Id","FollowUp Status","FollowUp Remarks","Next FollowUp Date",
+                                             "Sender Name", "Sender Email", "Mobile", "Subject","Quotation"
+                                            // , "Enq Date Time"
+                                             ,"no-sort_View"},
+                JsonUrl = Url.Action("InProgressLeadDataTable", "InProgressLead", new { area = "Master" })
+            };
+            return PartialView(model);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> InProgressLeadDataTable()
+        {
+            var assignLeads = await (from al in _dbContext.AssingLeads
+                                     where (al.FkUserId == Convert.ToInt64(User.FindFirstValue("UserId")) &&  (al.LeadStatus !=4 && al.LeadStatus !=6))
+                                     join lm in _dbContext.LeadMaster on al.FkLeadId equals lm.Id
+                                     join us in _dbContext.Users on al.FkUserId equals us.UserId
+                                     let fw = al.LeadFollowUpDetails.OrderByDescending(x => x.CreatedDate).FirstOrDefault()
+                                     select new List<string>()
+                             {
+                                  "<a onclick=fnAddUpdate("+al.AssignLeadId+"); title='Follow-Up' class='btn btn-default btn-icon btn-circle btn-lg'>"+ al.AssignLeadId.ToString()+"</a>",
+                                    al.LeadStatus==1?"Assigned"
+                                                    :al.LeadStatus==2?"Inprogress"
+                                                    :al.LeadStatus==3?"<a onclick=fnConvertToCustomer("+al.FkLeadId+"); title='Convert To Customer' class='btn btn-link'>Done</a>"
+                                                    :al.LeadStatus==4?"Close"
+                                                    :al.LeadStatus==5?"Hold"
+                                                    :"Customer",
+                                    !string.IsNullOrEmpty(fw.FollowUpComment)?fw.FollowUpComment.Length>100?
+                                    "<p title='"+fw.FollowUpComment+"'>"+ fw.FollowUpComment.Substring(0, 100) +"</p>":fw.FollowUpComment:String.Empty,
+                                    fw.FollowUpDate.HasValue ? fw.FollowUpDate.Value.ToString("dd/MM/yyyy HH:mm") : "[N/A]",
+                                    lm.Sendername,
+                                    lm.Senderemail,
+                                    lm.Mob,
+                                    !string.IsNullOrEmpty(lm.Subject)?lm.Subject.Length>100?
+                                    "<p title='"+lm.Subject+"'>"+ lm.Subject.Substring(0, 100) +"</p>":lm.Subject:String.Empty,
+                                    //  lm.LogTime?.ToString(),
+                                    "<a onclick=fnSendQuotation("+lm.Id+"); title='Send Quotation' class='btn btn-default btn-icon btn-circle btn-lg'><i class='fa fa-send-o'></i></a>  " +
+                                    "<a onclick=fnViewQuotation("+lm.Id+"); title = 'View Quotations' class='btn btn-default btn-icon btn-circle btn-lg'><i class='fa fa-code-fork'></i></a>",
+                                    "<a onclick=fnViewLeadDetails("+lm.Id+"); title='View Lead Details' class='btn btn-default btn-icon btn-circle btn-lg'><i class='fa fa-edit'></i></a>"
+                             }).ToArrayAsync();
+
+            return Ok(new { data = assignLeads });
+        }
+    }
+}
